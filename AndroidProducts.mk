1f0fc74b369a035e66794a45dd7f8e82f3bbaef4
YD_L <134907640+YD1742484575@users.noreply.github.com>
2026-09-06 03:09:18 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_RMX2205CN.mk
+    $(LOCAL_DIR)/twrp_RMX2205CN.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_RMX2205CN-user \
-    omni_RMX2205CN-userdebug \
-    omni_RMX2205CN-eng
+    twrp_RMX2205CN-user \
+    twrp_RMX2205CN-userdebug \
+    twrp_RMX2205CN-eng
