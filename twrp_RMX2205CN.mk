1dadfb1f05740fd3687c515fcf7fd58b6b73c590
YD_L <134907640+YD1742484575@users.noreply.github.com>
2026-09-06 03:10:46 +0800

    Update twrp_RMX2205CN.mk

--- twrp_RMX2205CN.mk
+++ twrp_RMX2205CN.mk
@@ -9,8 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from RMX2205CN device
 $(call inherit-product, device/realme/RMX2205CN/device.mk)
